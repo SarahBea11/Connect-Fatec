5aafa5f7fb341cb781124a3da827ecdb4dc8470e
SarahBea11 <sarahbealimapina@gmail.com>
2026-06-10 14:32:52 -0300

    Criação da API de frases motivacionais

---
+++ services/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const getAdvice = async () => {
+  try {
+    const response = await axios.get("https://api.adviceslip.com/advice");
+    return response.data.slip.advice;
+  } catch (error) {
+    throw error;
+  }
+};
