b4199ed2271fd0b9227050a076020daeeb61c739
SarahBea11 <sarahbealimapina@gmail.com>
2026-06-10 15:01:46 -0300

    Implementação do useReducer

---
+++ reducer/appReducer.js
@@ -0,0 +1,18 @@
+export const appReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_REGISTRO":
+      return {
+        ...state,
+        registros: [...state.registros, action.payload],
+      };
+
+    case "FAVORITAR":
+      return {
+        ...state,
+        favoritos: [...state.favoritos, action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
